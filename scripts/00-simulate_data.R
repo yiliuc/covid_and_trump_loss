d418a259f5399ff2a8865cb34ac2dad04f96bdd5
Yiliu Cao <l1023498789@gmail.com>
2024-01-12 13:57:42 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -9,10 +9,19 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# This is a simple simulation of the analysis data
+# I used number to represent the counties
+
+data <- tibble(county = sample(c(1:3000)),
+               vote_demo = runif(3000, min = 0, max = 1),
+               vote_rep = 1 - vote_demo,
+               dpc = runif(3000, min = 0, max = 1),
+               insurance = runif(3000, min = 0, max = 1),
+               higher_education = runif(3000, min = 0, max = 0.5),
+               winning_party = ifelse(vote_demo > vote_rep, "Democrat",
+                                      "Republican"))
 
 
 
